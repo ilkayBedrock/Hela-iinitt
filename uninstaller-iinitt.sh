afda8b427d026b8af041e318eb7deda91d01b377
ilkayBedrock <ilkayxda@aol.com>
2026-03-16 15:02:08 +0300

    Update uninstaller-iinitt.sh

--- uninstaller-iinitt.sh
+++ uninstaller-iinitt.sh
@@ -10,7 +10,7 @@ echo "SYSHALT after the uninstall message."
 sleep 5
 
 # 3. Remove hela binary with sudo
-echo "[*] Removing hela binary: /usr/local/bin/hela"
+echo "[*] Removing hela binary executable: /usr/local/bin/hela"
 sudo rm -rf /usr/local/bin/hela*
 
 # 4. Remove tmp files
