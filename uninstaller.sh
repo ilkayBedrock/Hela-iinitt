bc496856da5777c72ee16c9f5c0de2e36529c363
ilkayBedrock <ilkayxda@aol.com>
2026-03-16 14:55:03 +0300

    Update uninstaller.sh

--- uninstaller.sh
+++ uninstaller.sh
@@ -8,12 +8,12 @@ systemctl --user disable hela-watchdog.service 2>/dev/null
 rm -rf ~/.config/systemd/user/hela-watchdog.service
 rm -rf ~/.local/bin/hela-watchdog.sh
 
-# 2. Reload systemd user
+# 2. Reload systemd user daemon
 systemctl --user daemon-reload 2>/dev/null
 systemctl --user reset-failed 2>/dev/null
 
 # 3. Remove hela binary with sudo
-echo "[*] Removing hela binary: /usr/local/bin/hela"
+echo "[*] Removing hela binary executable: /usr/local/bin/hela"
 sudo rm -rf /usr/local/bin/hela*
 
 # 4. Remove tmp files
