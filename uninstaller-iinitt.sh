341ac3535e15654a5d1e36888f563884ec9d13e9
ilkayBedrock <ilkayxda@aol.com>
2026-03-16 15:01:41 +0300

    Add files via upload

---
+++ uninstaller-iinitt.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "[*] Uninstalling Hela Flash Tool..."
+
+# 1. iinitt info msg
+echo "Please delete the hela-watchdog service from the iinitt.c file and recompile the code"
+
+# 2. Info the uninstaller
+echo "SYSHALT after the uninstall message."
+sleep 5
+
+# 3. Remove hela binary with sudo
+echo "[*] Removing hela binary: /usr/local/bin/hela"
+sudo rm -rf /usr/local/bin/hela*
+
+# 4. Remove tmp files
+echo "[*] Removing temporary files..."
+sudo rm -rf /tmp/hela_*
+
+# 5. Confirm
+echo "[+] Hela has been completely uninstalled."
